31e1a3378c0e66c050bb298e7e8d6207497ea9ed
dung <dunglevan2001@gmail.com>
2020-12-12 23:43:46 +0000

    Backend now save uploaded excel files to Resources/UploadedFiles

--- com.DataConsolidation/src/main/java/Group1/com/DataConsolidation/UploadHandlerController/UploadController.java
+++ com.DataConsolidation/src/main/java/Group1/com/DataConsolidation/UploadHandlerController/UploadController.java
@@ -7,20 +7,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 @RestController
 @CrossOrigin(value = "http://localhost:3000")
 public class UploadController {
+    private static String UPLOADED_FOLDER = "src/main/resources/UploadedFiles/";
     private static final Logger logger = Logger.getLogger(UploadController.class.getName());
     private static String MessageToShow = "";
-    @PostMapping("/upload")
+    @PostMapping("/upload") // Handle Post Request sent by the React Client (save Uploaded files into resources)
     public ResponseEntity<String> uploadData(@RequestParam("file") MultipartFile file) throws Exception {
         if (file == null) {
             throw new RuntimeException("You must select the a file for uploading");
         }
+
         InputStream inputStream = file.getInputStream();
+        byte[] buffer = new byte[inputStream.available()];
+        //Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+        inputStream.read(buffer);
+        File targetFile = new File(UPLOADED_FOLDER + "targetFile.xlsx"); // All Files will be stored as xlsx ?
+        try (OutputStream outStream = new FileOutputStream(targetFile)) {
+            outStream.write(buffer);
+        }
+
+
+        //Logging information into the console. (just for Debugging)
         String originalName = file.getOriginalFilename();
         String name = file.getName();
         String contentType = file.getContentType();
